3f41decb0712c040d3a581038a61e4d7f070d8d4
Brian Rainey <brainey22@gmail.com>
2020-12-04 00:15:46 -0600

    hover and accordion for body

---
+++ js/index.js
@@ -0,0 +1,12 @@
+console.log("this worked");
+
+$(".subLevel").click(function() {
+    $(this).toggleClass("active");
+    $(this).siblings(".accordion").find(".bodyParagraph").toggleClass("expand");
+});
+
+$(".subLevel2").click(function() {
+    $(this).toggleClass("active");
+    $(this).siblings(".accordion").find(".bodyParagraph").toggleClass("expand");
+    $(this).siblings(".accordion").find(".workEx").toggleClass("expand"); 
+});
